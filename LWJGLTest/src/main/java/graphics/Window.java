92ca8a6111af96f77a3b6b05646a9df42c85c049
AndrewRunke <andrewrunke@gmail.com>
2025-06-26 01:21:50 -0400

    Added Window Mode Changing

--- LWJGLTest/src/main/java/graphics/Window.java
+++ LWJGLTest/src/main/java/graphics/Window.java
@@ -10,6 +10,13 @@ import java.nio.*;
 import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL13.GL_TEXTURE0;
+import static org.lwjgl.opengl.GL13.glActiveTexture;
+import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
+import static org.lwjgl.opengl.GL15.glBindBuffer;
+import static org.lwjgl.opengl.GL15.glBufferData;
+import static org.lwjgl.opengl.GL15.glGenBuffers;
 import static org.lwjgl.opengl.GL20.*;
 import static org.lwjgl.opengl.GL30.*;
 import static org.lwjgl.system.MemoryStack.*;
@@ -54,46 +61,22 @@ public class Window {
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // the window will be resizable
         glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
-
+        
         // Create the window
-        window = glfwCreateWindow(screenWidth, screenHeight, title, NULL, NULL);
-        if ( window == NULL )
-            throw new RuntimeException("Failed to create the GLFW window");
+        changeWindowedMode(2, screenWidth, screenHeight, title);
 
-        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
-        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-            if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE ) {
-                glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
-            }
-        });
+        initGL();
 
-        // Get the thread stack and push a new frame
-        try ( MemoryStack stack = stackPush() ) {
-            IntBuffer pWidth = stack.mallocInt(1); // int*
-            IntBuffer pHeight = stack.mallocInt(1); // int*
+        // try{
+        //     Thread.sleep(2000);
+        // } catch(Exception e){
+        //     e.printStackTrace();
+        // }
+        // changeWindowedMode(2, screenWidth, screenHeight, "test");
 
-            // Get the window size passed to glfwCreateWindow
-            glfwGetWindowSize(window, pWidth, pHeight);
+    }
 
-            // Get the resolution of the primary monitor
-            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
-            // Center the window
-            glfwSetWindowPos(
-                    window,
-                    (vidmode.width() - pWidth.get(0)) / 2,
-                    (vidmode.height() - pHeight.get(0)) / 2
-            );
-        } // the stack frame is popped automatically
-
-        // Make the OpenGL context current
-        glfwMakeContextCurrent(window);
-        // Enable v-sync
-        glfwSwapInterval(1);
-
-        // Make the window visible
-        glfwShowWindow(window);
-
+    private void initGL(){
         GL.createCapabilities();
 
         shader = new Shader("src/main/resources/final_pass_vert.glsl", "src/main/resources/final_pass_frag.glsl") {
@@ -170,6 +153,67 @@ public class Window {
         }else{
             System.out.println(pass);
         }
+    }
+
+    //Change Window Mode between Fullscreen, Borderless Windowed Fullscreen, and Windowed
+    // 0: Fullscreen, 1: Borderless Windowed Fullscreen, 2: Windowed
+    public void changeWindowedMode(int mode, final int screenWidth, final int screenHeight, String title){
+        if ( window != NULL ){
+            glfwDestroyWindow(window);
+        }
+        switch (mode) {
+            case 0: //Fullscreen
+                window = glfwCreateWindow(screenWidth, screenHeight, title, glfwGetPrimaryMonitor(), NULL);
+                break;
+            case 1: //Borderless Windowed Fullscreen
+                glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // the window will be resizable
+                glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
+                window = glfwCreateWindow(screenWidth, screenHeight, title, NULL, NULL);
+                break;
+            default: //Windowed
+                glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+                glfwWindowHint(GLFW_DECORATED, GLFW_TRUE);
+                window = glfwCreateWindow(screenWidth/2, screenHeight/2, title, NULL, NULL);
+                break;
+        }
+
+        if ( window == NULL )
+            throw new RuntimeException("Failed to create the GLFW window");
+
+        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
+        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+            if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE ) {
+                glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
+            }
+        });
+
+        // Get the thread stack and push a new frame
+        try ( MemoryStack stack = stackPush() ) {
+            IntBuffer pWidth = stack.mallocInt(1); // int*
+            IntBuffer pHeight = stack.mallocInt(1); // int*
+
+            // Get the window size passed to glfwCreateWindow
+            glfwGetWindowSize(window, pWidth, pHeight);
+
+            // Get the resolution of the primary monitor
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            // Center the window
+            glfwSetWindowPos(
+                    window,
+                    (vidmode.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            );
+        } // the stack frame is popped automatically
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(window);
+        // Enable v-sync
+        glfwSwapInterval(1);
+
+        // Make the window visible
+        glfwShowWindow(window);
+        
     }
 
 }
